04319ad497d163541a7031110bead480f01b4291
Jawaid Bazyar <bazyar@gmail.com>
2025-08-31 14:29:44 -0400

    fix - after api change, we need to print debug trace lines here

--- apps/gstrace/main.cpp
+++ apps/gstrace/main.cpp
@@ -2,11 +2,14 @@
 #include "debugger/trace.hpp"
 
 int main(int argc, char **argv) {
+    char *tmsg;
+
     system_trace_buffer trace_buffer(100000);
     trace_buffer.read_from_file("trace.bin");
-
+    
     for (size_t i = 0; i < trace_buffer.size; i++) {
         system_trace_entry_t *entry = trace_buffer.get_entry(i);
-        trace_buffer.decode_trace_entry(entry);
+        tmsg = trace_buffer.decode_trace_entry(entry);
+        printf("%s\n", tmsg);
     }
 }
