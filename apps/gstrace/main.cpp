f9b9ee22a784653131bf75905a67c8f6eef6cde1
Jawaid Bazyar <bazyar@gmail.com>
2026-01-12 18:26:56 -0500

    allow specifying trace file name

--- apps/gstrace/main.cpp
+++ apps/gstrace/main.cpp
@@ -5,8 +5,14 @@ int main(int argc, char **argv) {
     char *tmsg;
 
     processor_type cputype = PROCESSOR_6502;
+    const char *trace_filename = nullptr;
     
-    for (int i = 1; i < argc; i++) {
+    if (argc < 3) {
+        printf("usage: gstrace 6502|65c02|65816e tracefilename.bin\n");
+        return 1;
+    }
+    
+    for (int i = 1; i < argc - 1; i++) {
         if (strcmp(argv[i], "6502") == 0) {
             cputype = PROCESSOR_6502;
         } else if (strcmp(argv[i], "65c02") == 0) {
@@ -14,12 +20,15 @@ int main(int argc, char **argv) {
         } else if (strcmp(argv[i], "65816e") == 0) {
             cputype = PROCESSOR_65816;
         } else {
-            printf("usage: gstrace [6502|65c02|65816e]\n");
+            printf("usage: gstrace 6502|65c02|65816e tracefilename.bin\n");
+            return 1;
         }
     }
+    
+    trace_filename = argv[argc - 1];
 
     system_trace_buffer trace_buffer(100000, cputype);
-    trace_buffer.read_from_file("trace.bin");
+    trace_buffer.read_from_file(trace_filename);
     
     for (size_t i = 0; i < trace_buffer.size; i++) {
         system_trace_entry_t *entry = trace_buffer.get_entry(i);
