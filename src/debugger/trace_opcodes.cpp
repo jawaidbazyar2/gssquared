a88a5bd81b027bcf603474d25791d938a7b4b914
Jawaid Bazyar <bazyar@gmail.com>
2026-01-31 12:09:35 -0500

    mvn/mvp is 3 bytes not 2

--- src/debugger/trace_opcodes.cpp
+++ src/debugger/trace_opcodes.cpp
@@ -43,7 +43,7 @@ const address_mode_entry address_mode_formats[] = {
     { "$%04X", 3},       // REL_L
     { "[$%04X]", 3},       // ABS_IND_LONG
     { "#$%04X", 2},       // IMM_S
-    { "$%02X,$%02X", 2},       // MOVE
+    { "$%02X,$%02X", 3},       // MOVE
 };
 
 const disasm_entry disasm_table[256] = { 
