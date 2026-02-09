ce5b391a4d4e31441ddba31b60273a58d293711c
Jawaid Bazyar <bazyar@gmail.com>
2026-02-09 13:51:34 -0500

    comment

--- src/util/printf_helper.hpp
+++ src/util/printf_helper.hpp
@@ -1,5 +1,10 @@
 #pragma once
 
+/* This is a dirty hack to work around the fact that on linux 
+    uint64_t is unsigned long long and on MacOS it is unsigned long, and clang-linux "helpfully"
+    warns if you try to use %llu on a uint64_t.
+*/
+
 template<typename T>
 inline unsigned long long u64_t(T value) {
     static_assert(sizeof(T) == 8, "Type must be 64-bit");
