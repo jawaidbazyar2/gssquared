f36805e3dc81545718cd53088a96a6b67041eaa3
Jawaid Bazyar <bazyar@gmail.com>
2026-02-26 09:41:46 -0500

    remove (unneeded) dependence on cpu

--- src/devices/cassette/cassette.cpp
+++ src/devices/cassette/cassette.cpp
@@ -6,17 +6,15 @@
 // This basically does nothing right now. Just return floating bus state.
 
 uint8_t cassette_memory_read(void *context, uint32_t address) {
-    cpu_state *cpu = (cpu_state *)context;
-    //return cpu->mmu->floating_bus_read() & 0x7F;
-    return cpu->mmu->floating_bus_read() | 0x80; // "real apple //e enhanced with nothing plugged into cassette, reads 80 from C060" - Arekkusu
+    cassette_state_t *cassette_state = (cassette_state_t *)context;
+
+    return cassette_state->mmu->floating_bus_read() | 0x80; // "real apple //e enhanced with nothing plugged into cassette, reads 80 from C060" - Arekkusu
 }
 
-
 void init_mb_cassette(computer_t *computer, SlotType_t slot) {
-    cpu_state *cpu = computer->cpu;
+    cassette_state_t *cassette_state = new cassette_state_t;
+    cassette_state->mmu = computer->mmu;
     
-    //set_module_state(cpu, MODULE_CASSETTE, cassette_state);
-
-    computer->mmu->set_C0XX_read_handler(0xC060, { cassette_memory_read, cpu });
-    computer->mmu->set_C0XX_read_handler(0xC068, { cassette_memory_read, cpu });
+    computer->mmu->set_C0XX_read_handler(0xC060, { cassette_memory_read, cassette_state });
+    computer->mmu->set_C0XX_read_handler(0xC068, { cassette_memory_read, cassette_state });
 }
--- src/devices/cassette/cassette.hpp
+++ src/devices/cassette/cassette.hpp
@@ -6,4 +6,8 @@
 
 // This basically does nothing right now. Just return floating bus state.
 
+struct cassette_state_t {
+    MMU_II *mmu = nullptr;
+};
+
 void init_mb_cassette(computer_t *computer, SlotType_t slot);
